70e166d301ad964671a3336195f8d9370bbd39da
Scott <2251386+PolarisScientia@users.noreply.github.com>
2020-03-02 10:11:27 -0500

    Update pgm_segment.py

--- pgm_segment.py
+++ pgm_segment.py
@@ -20,9 +20,18 @@ class Cell:
         self.row = row
         self.col = col
         self.container = container
+        self.value = self.setValue()
 
     def setContainer(self, container):
         self.container = container
+    
+    def setValue(self):
+        if not self.container:
+            return -1
+        return self.container.segMan.getVal(self.row, self.col)
+    
+    def getValue(self):
+        return self.value
 
 class Box:
     def __init__(self, topLeft=(-1,-1), botRight=(-1,-1)):
@@ -34,6 +43,14 @@ class Box:
         dimy = abs(self.botRight[1] - self.topLeft[1] + 1)
         return dimx*dimy
 
+class Box4(Box):
+    def __init__(self, topLeft=(-1,-1), botLeft=(-1,-1), topRight=(-1,-1), botRight=(-1,-1)):
+        self.topLeft = topLeft
+        self.topRight = topRight
+        self.botLeft = botLeft
+        self.botRight = botRight
+
+    
 class Segment:
     """A segmented area from the image file."""
     def __init__(self, segMan, cells=[]):
@@ -53,6 +70,15 @@ class Segment:
             right = max(right, cell.col)
         self.boundingBox = Box((left, top), (right, bot))
 
+    #def setBounds4(self):
+        #initialize each coordinate to it's polar opposite
+    #    topLeft = [sys.maxsize, sys.maxsize]
+    #    botLeft = [0, sys.maxsize]
+    #    topRight = [sys.maxsize, 0]
+    #    botRight = [0, 0]
+
+    #    for cell in self.cells:
+            
     def size(self, update=1):
         """Returns the square area of a bounding box containing all Cells within the Segment object."""
         if update:
@@ -104,8 +130,9 @@ class Segment:
                         self.grow(nCell)
 
 
+
 class SegmentManager:
-    """A manager for individual segments."""
+    """A singleton for managing the image. Has facilities for segmentation and other image modifications."""
     def __init__(self, raster, segments=[], unknown=205, free=254, occupied=0):
         self.raster = raster
         self.segments = segments
@@ -127,8 +154,13 @@ class SegmentManager:
             print("Pruned Segment #{}:".format(i),segment.size(),"({},{}) [{}]".format(segment.boundingBox.topLeft, segment.boundingBox.botRight, len(segment.cells)))
             i = i+1
     
+    def inBounds(self, row, col):
+        if row > self.max_row or col > self.max_col or row <0 or col < 0:
+            return 0
+        return 1
+
     def getVal(self, row, col):
-        if row > self.max_row or col > self.max_col or row < 0 or col < 0:
+        if not self.inBounds(row, col):
             return -1 # should protect against out of range access
         return self.raster[row][col]
 
@@ -154,6 +186,7 @@ class SegmentManager:
         self.segments.append(segment)
 
     def scanRaster(self):
+        """Scans through raster and grows segments on every occupied pixel."""
         row = 0
         for line in self.raster: # each row in the image
             col = 0 # reset column count each row
@@ -164,6 +197,7 @@ class SegmentManager:
             row = row + 1
     
     def prunePercentileSize(self, percentile):
+        """Prunes segments that are below a percentile size thresholding."""
         #TODO: Add overwriting capability (aka thresholding smaller segments out of the image)
         size = []
         for segment in self.segments:
@@ -185,6 +219,64 @@ class SegmentManager:
         self.prunedSegments.append(segment)
         self.segments.remove(segment)
 
+                    
+def cropRaster(raster, cropval=205):
+    right = bottom = 0
+    top = left = sys.maxsize
+    row = col = 0
+    frow = fcol = 0
+    for line in raster: #top-down, left-right scan
+        col = 0
+        frow_last = frow
+        frow = 0 #flagged row (with data in it, for detecting tail end)
+        for value in line:
+            fcol_last = fcol
+            fcol = 0
+            if value != cropval: # real data
+                frow = fcol = 1
+                if top > row: #if top thresh is lower than current line
+                    top = max(0, row)
+                if left > col:
+                    left = max(0, col)
+            if (fcol_last) and not fcol and (right < col):
+                right = col
+            col = col+1
+        if (frow_last) and not frow and (bottom < row):
+            bottom = row
+        row = row+1
+    print("\nVertexes of crop:\n")
+    print("Left:", left, " Right:", right)
+    print("\nTop:", top, "Bottom:", bottom)
+    crop1 = raster[top:bottom]
+    crop2 = []
+    for row in crop1:
+        crop2.append(row[left:right])
+    return crop2
+
+def dilateRaster(raster, size=5, occupied=0):
+    """Dilate the raster by convolution of size x size matrix of 1's
+        size parameter should be an odd value."""
+    i = int((size-1)/2)
+    row = 0
+    max_row = len(raster)-1
+    max_col = len(raster[1])-1
+    toDilate = []
+    for line in raster: #collect pixels to dilate first
+        col = 0
+        for value in line:
+            if value == occupied:
+                for rowmod in range(-i, i+1):
+                    for colmod in range(-i, i+1):
+                        if row+rowmod > 0 and col+colmod > 0 and row+rowmod <= max_row and col+colmod <= max_col:
+                            cVal = raster[row+rowmod][col+colmod]
+                            if cVal != occupied:
+                                toDilate.append((row+rowmod, col+colmod))
+            col = col+1
+        row = row+1
+    for tup in toDilate: #iterate through collected pixel tuples to set values
+        raster[tup[0]][tup[1]] = occupied
+    return raster
+
 def read_pgm(pgmf):
     """Return a raster of integers from a PGM as a list of lists."""
     assert pgmf.readline() == b'P5\n'
@@ -204,23 +296,49 @@ def read_pgm(pgmf):
         raster.append(row)
     return raster
 
-def processFile(inputfile):
-    with open(inputfile, 'rb') as f:
-        raster = read_pgm(f) #raster list of rows
-        print("Raster size:", len(raster),"x",len(raster[1]))
-        segMan = SegmentManager(raster)
-        segMan.scanRaster()
-        segMan.prunePercentileSize(80.0) #,0)
-        segMan.printInfo()
-        #all data is deleted at the end of this function!!!
+def processFile(inputfile, outputfile, pgm, crop, dilate):
+    if inputfile:
+        with open(inputfile, 'rb') as f:
+            raster = read_pgm(f) #raster list of rows
+            if crop:
+                print("Raster cropped.")
+                raster = cropRaster(raster, 205)
+            if dilate:
+                print("Raster dilated.")
+                raster = dilateRaster(raster, dilate)
+
+            print("Raster size:", len(raster),"x",len(raster[1]))
+            segMan = SegmentManager(raster)
+            segMan.scanRaster()
+            segMan.prunePercentileSize(80.0) #,0)
+            segMan.printInfo()
+            if outputfile:
+                if pgm:
+                    with open(outputfile, 'w+b') as o:
+                        flat_raster = [item for sublist in raster for item in sublist]
+                        height = len(raster)
+                        width = len(raster[1])
+                        o.write(bytes('P5' + '\n' + '# CREATOR: pgm_segment.py 0.01\n', encoding='utf8'))
+                        o.write(bytes(str(width) + ' ' + str(height) + '\n' + str(255) + '\n', encoding='utf8'))
+                        rasterbytes = bytes(flat_raster)
+                        o.write(rasterbytes)
+                else:
+                    with open(outputfile, 'w+') as o:
+                        for line in raster:
+                            for val in line:
+                                o.write(str(val) + ',')
+                            o.write('\n')
 
 def main(argv):
     inputfile = ''
     outputfile = ''
+    pgm = 0
+    crop = 0
+    dilate = 0
     try:
-        opts, args = getopt.getopt(argv,"i:o:",["ifile=","ofile="])
+        opts, args = getopt.getopt(argv,"hbcd:i:o:",["dilate=", "ifile=","ofile="])
     except getopt.GetoptError:
-        print(str(err))
+        print(str(getopt.err))
         sys.exit(2)
     for opt, arg in opts:
         if opt == '-h':
@@ -230,9 +348,19 @@ def main(argv):
             inputfile = arg
         elif opt in ("-o", "--ofile"):
             outputfile = arg
+        elif opt == '-b':
+            print("Outputting as PGM.")
+            pgm = 1
+        elif opt == '-c':
+            print("Cropping margins.")
+            crop = 1
+        elif opt in ("-d", "--dilate"):
+            print("Dilating raster prior to segmentation. Size of " + str(int(arg)))
+            dilate = int(arg)
+
     print("Input file is", inputfile)
     print("Output file is", outputfile)
-    processFile(inputfile)
+    processFile(inputfile, outputfile, pgm, crop, dilate)
 
 if __name__ == "__main__":
     main(sys.argv[1:])
